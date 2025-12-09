d59200eb3ec75c2d70edb7e6833818c88bf21bc0
LosungsJager <113644338+LosungsJager@users.noreply.github.com>
2025-12-09 19:22:32 -0300

    Create teste_rodas.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teste_rodas.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BHI260IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp
+public class teste_rodas extends LinearOpMode {
+    public DcMotor esq_f, dir_f, esq_t, dir_t = null;
+
+    IMU.Parameters myIMUparameters;
+
+    BHI260IMU imu;
+
+    public void rotacao() {
+        //Rotação motores tração.
+        esq_f.setDirection(DcMotor.Direction.FORWARD);
+        dir_f.setDirection(DcMotor.Direction.REVERSE);
+        esq_t.setDirection(DcMotor.Direction.FORWARD);
+        dir_t.setDirection(DcMotor.Direction.REVERSE);
+
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        esq_f = hardwareMap.get(DcMotor.class, "esq_f");
+        esq_t = hardwareMap.get(DcMotor.class, "esq_t");
+        dir_f = hardwareMap.get(DcMotor.class, "dir_f");
+        dir_t = hardwareMap.get(DcMotor.class, "dir_t");
+        imu = hardwareMap.get(BHI260IMU.class, "imu");
+
+        myIMUparameters = new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                        RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+                )
+        );
+
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+
+        imu.initialize(parameters);
+
+        if (gamepad1.options) {
+            imu.resetYaw();
+        }
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            double y = -gamepad1.left_stick_y; //o y é inverso, por isso o -
+            double x = gamepad1.left_stick_x * 1.1; //corrigir imperfeições do movimento lateral
+            double rx = gamepad1.right_stick_x;
+
+            if (Math.abs(y) < 0.05) y = 0; //medida de segurança caso o joystick acabe mexendo um pouco sozinho
+            if (Math.abs(x) < 0.05) x = 0;
+            if (Math.abs(rx) < 0.05) rx = 0;
+
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+            rotX = rotX * 1.1;
+
+            //O denominador é a maior potência do motor (valor absoluto) ou 1
+            // Isso garante que todas as potências mantenham a mesma proporção,
+            // mas somente se pelo menos uma estiver fora do intervalo [-1, 1]
+
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double esq_fPower = (rotY - rotX - rx) / denominator;
+            double esq_tPower = (rotY + rotX/1.4 - rx) / denominator;
+            double dir_fPower = (rotY + rotX + rx) / denominator;
+            double dir_tPower = (-rotY + rotX - rx) / denominator;
+
+            esq_f.setPower(esq_fPower);
+            esq_t.setPower(esq_tPower);
+            dir_f.setPower(dir_fPower);
+            dir_t.setPower(dir_tPower);
+
+
+        }
+    }
+}
